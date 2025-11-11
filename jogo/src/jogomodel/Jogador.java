5b31d9b53e816693e549b21bd2a9b501d2211f6a
Inesar16 <ines.arodrigues16@gmail.com>
2025-11-11 11:31:08 +0000

    upadte

--- jogo/src/jogomodel/Jogador.java
+++ jogo/src/jogomodel/Jogador.java
@@ -1,5 +1,9 @@
 package jogomodel;
 
 public class Jogador {
+	
+	private int x, y;
+	private int dx, dy;
+	private 
 
 }
--- jogo/src/jogomodel/jogo2D.java
+++ jogo/src/jogomodel/jogo2D.java
@@ -1,5 +1,18 @@
 package jogomodel;
 
-public class jogo2D {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class jogo2D extends JFrame {
+	
+	public static void main(String[] args) {
+        JFrame frame = new jogo2D();
+        frame.setTitle("Shadow");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 
 }
